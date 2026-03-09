5ee60bcd9915e222dbf5622eee8f713d21207201
cobontacobell-maker <cobontacobell@gmail.com>
2026-03-09 01:48:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function siguientePaso() {
     if (nombre) {
         document.getElementById('paso1').style.display = 'none';
         document.getElementById('paso2').style.display = 'block';
-        document.getElementById('mensaje').innerText = `¡Hola ${nombre}! Hola Sofia, mira te hago esta carta, si se puede llamar de una manera, ya que la verdad te quería desear un Feliz cumpleaños.
+        document.getElementById('mensaje').innerText = `Hola Sofia, mira te hago esta carta, si se puede llamar de una manera, ya que la verdad te quería desear un Feliz cumpleaños.
 Yo sé que no nos conocemos hace mucho, pero créeme que para mí eres ahora mismo una de las personas mas importantes en mi vida en estos momentos, y aunque pienses que es una exageración es muy cierto.
 Yo se que yo para ti no puedo ser alguien importante ya que no es que este en tu vida al 100% y se que no eres de encariñarte así de las personas o así me has dicho, pero espero en su momento poder llegar a ganarme tu cariño.
 Y hago estop para poder expresar mis sentimientos de una manera distinta, yo se que hoy muchos amigos te darán una felicitación y imagino que yo seré del montón igualmente, pero me alegra poder estar en una fecha muy especial para ti que es tu cumple.
